6bff6c2d9c570babdbb5ead4ef2925fb368d00c0
pramudithamendis <pramudithamendis444@gmail.com>
2024-01-31 20:22:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+numbers = [78, 74, 82, 66, 91, 71, 64, 88, 55, 80, 51, 74, 82, 75, 16, 78, 84, 79, 71, 83 ]
+
+
+max = Math.max(...numbers)
+min = Math.min(...numbers)
+
+range = max - min
+numberOfClasses = 8
+
+Aclass = range/numberOfClasses
+
+console.log(Aclass)
+
+Aclass = Math.floor(Aclass)
+Aclass = 10
+
+
+
+numbers.forEach(element => {
+    
+});
+
+class1lowerlimit = min - 2
+class1lowerlimit = class1lowerlimit + 0.5
+console.log(class1lowerlimit)
+class1upperlimit = class1lowerlimit + classes
+console.log(class1upperlimit)
+
+class1midpoint = (class1lowerlimit + class1upperlimit)/2
+console.log(class1midpoint)
+
